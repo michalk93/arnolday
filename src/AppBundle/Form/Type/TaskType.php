a7fc0cf25d7d51c41dd8feece30e894fe63ee56d
Michał Kolbusz <michalkolbusz@mailplus.pl>
2015-03-11 20:45:41 +0100

    Adding and editing tasks

---
+++ src/AppBundle/Form/Type/TaskType.php
@@ -0,0 +1,19 @@
+<?php
+namespace AppBundle\FormType;
+/**
+ * Created by PhpStorm.
+ * User: kolbusz
+ * Date: 08.03.15
+ * Time: 14:16
+ */
+use \Symfony\Component\Form\AbstractType;
+use \Symfony\Com
+
+class TaskType extends AbstractType{
+
+    public function builFo
+
+    public function getName(){
+        return 'task_type';
+    }
+}
