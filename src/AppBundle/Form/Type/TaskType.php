e380afc8fced74d4e2359b5a12a07988a9eed2f5
Michał Kolbusz <michalkolbusz@mailplus.pl>
2015-03-12 21:42:33 +0100

    TaskType recommit

--- src/AppBundle/Form/Type/TaskType.php
+++ src/AppBundle/Form/Type/TaskType.php
@@ -1,5 +1,5 @@
 <?php
-namespace AppBundle\FormType;
+namespace AppBundle\Form\Type;
 /**
  * Created by PhpStorm.
  * User: kolbusz
@@ -7,13 +7,41 @@ namespace AppBundle\FormType;
  * Time: 14:16
  */
 use \Symfony\Component\Form\AbstractType;
-use \Symfony\Com
+use \Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class TaskType extends AbstractType{
 
-    public function builFo
+    private $mode = 'create';
 
+    public function __construct($mode = 'create'){
+        $this->mode = $mode;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder->add('done', 'checkbox', ['required' => false])
+            ->add('priority', 'choice', ['choices' => [0,1,2]])
+            ->add('category', 'entity', ['class' => 'AppBundle:Category'])
+            ->add('assignee', 'entity', ['class' => 'AppBundle:User']);
+
+        if($this->mode == 'create'){
+            $builder->add('content', 'text')
+                ->add('dueDate', 'date');
+        }else if($this->mode == 'edit'){
+            $builder->add('content', 'text', ['disabled' => true])
+                ->add('dueDate', 'date', ['disabled' => true]);
+
+        }
+
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver){
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Task',
+        ]);
+    }
+    
     public function getName(){
-        return 'task_type';
+        return 'task';
     }
 }
