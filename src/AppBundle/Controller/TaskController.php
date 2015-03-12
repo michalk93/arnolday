1a06bd42dc9edcc10a1ef4c26b7f6c74bc2b3bd0
Michał Kolbusz <michalkolbusz@mailplus.pl>
2015-03-12 14:13:18 +0100

    Task functions (adding and editing) added

---
+++ src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Task;
+use AppBundle\Form\Type\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * @Route("task/index", name="task-index")
+     */
+    public function indexAction(){
+        $tasks = $this->getDoctrine()->getRepository("AppBundle:Task")->findAll();
+        return $this->render('AppBundle:task:index.html.twig', ['tasks' => $tasks]);
+
+    }
+    /**
+     * @Route("/task/add", name="task-add")
+     * @Template("AppBundle:task:addTask.html.twig")
+     */
+    public function addTaskAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $task = new Task();
+        $task->setCreatedBy($em->find("AppBundle:User", 1));
+        $form = $this->createForm(new TaskType(), $task);
+
+        if($form->handleRequest($request)->isValid()){
+            $em->persist($task);
+            $em->flush();
+            return $this->render('AppBundle:task:index.html.twig');
+        }
+
+        return $this->render('add.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/task/edit/{id}", name="task-edit")
+     * @Template("AppBundle:task:editTask.html.twig")
+     */
+    public function editTaskAction(Task $task, Request $request)
+    {
+        $form = $this->createForm(new TaskType(), $task);
+        if($form->handleRequest($request)->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->render('AppBundle:task:index.html.twig');
+        }
+
+        return $this->render('edit.html.twig', ['form' => $form->createView()]);
+    }
+    
+    
+    
+
+}
