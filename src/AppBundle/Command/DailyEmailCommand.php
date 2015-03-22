f76100b50034274a5d8facdceeddf0f6c7dbbf23
michalk93 <kolmichal@o2.pl>
2015-03-22 12:57:56 +0100

    Console command to send notifications with tasks for next day to each user

---
+++ src/AppBundle/Command/DailyEmailCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use AppBundle\Entity\User;
+use Symfony\Component\Validator\Constraints\DateTime;
+use \DateTime;
+
+
+class DailyEmailCommCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('email:daily')
+            ->setDescription('Send email with tasks for next day to each user');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $users = $em->getRepository('AppBundle:User')
+        ->findAll();
+        foreach($users as $user){
+            $tasks = $em->getRepository('AppBundle:Task')->getTasksForNextDay();
+            if($tasks){
+                $mailer = $this->getContainer()->get('notification_mailer');
+                $mailer->createDailyMessage($user, $tasks);
+                $mailer->send();
+            }
+        }
+    }
+}
